f6b282bce32d083ce886139625b53a7027ffc0ec
Paulo <gabrielbsantana61@gmail.com>
2025-03-05 23:35:41 -0300

    commit de segurança

---
+++ tipos-variaveis/src/OperadoresJava.java
@@ -0,0 +1,45 @@
+public class OperadoresJava {
+    public static void main(String[] args) {
+        /**boolean condicao1=true;
+
+        boolean condicao2=true;
+
+        if(condicao1 && (7 > 4 && 10 < 5)){
+            System.out.println("as duas condições são verdadeira");
+        }
+
+        if (condicao1 || condicao2){
+            System.out.println("Uma das condições é verdadeira");
+        }
+
+        System.out.println("fim");**/
+
+        /**String nome1 = "Gabriel";
+        String nome2 = new String("Gabriel");
+
+        System.out.println(nome1.equals(nome2));
+
+
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        if(numero1 == numero2){  //true
+        System.out.println("a nossa codição é verdadeira");
+        }
+            
+        System.out.println("numero1 é igual a numero2? " + simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("numero1 é diferente a numero2? " + simNao);
+
+        simNao = numero1 > numero2;
+
+        System.out.println("numero1 é maior a numero2? " + simNao);**/
+
+
+    }
+}
