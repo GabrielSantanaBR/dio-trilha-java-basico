7368fec775098291980535894b15c760d1bf4e35
Paulo <gabrielbsantana61@gmail.com>
2025-03-07 15:19:45 -0300

    commit fim de tarefa

---
+++ java-terminal1/src/AboutMe.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class AboutMe {
+    public static void main(String[] args) {
+        //crinado o objeto scaner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Digite seu sobrenome: ");  
+        String sobrenome = scanner.nextLine();
+
+        System.out.println("Digite sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite sua altura: ");
+        double altura = scanner.nextDouble();
+
+        System.out.println("Ola, me chamo " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos ");
+        System.out.println("Minha altura é " + altura + "cm ");
+        
+    }
+
+}
--- java-terminal1/src/MinhaClasse.java
+++ java-terminal1/src/MinhaClasse.java
@@ -2,4 +2,4 @@ public class MinhaClasse {
     public static void main(String[] args) {
         System.out.println("Oi, fui executado pelo Terminal!");
     }
-}
+} 
