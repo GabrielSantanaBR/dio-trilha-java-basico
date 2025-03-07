39be1a3cea5d62f3c232fa481908fab96fbf7822
Paulo <gabrielbsantana61@gmail.com>
2025-03-07 14:10:19 -0300

    commit dia 5

---
+++ Java-terminal/src/Calculadora.java
@@ -0,0 +1,22 @@
+/**
+ * <h1>Calculadora</h1>
+ * A Calculadora realiza operações matematicas entre númers inteiros
+ * <p>
+ * <b>Nota:</b> Leia antentamente a documentação desta classes
+ * para desfrutar dos recursos oferecidos pelo autor
+ * 
+ * @author Gabriel Santana
+ * @version 1.0
+ * @since 01/01/2025
+ */
+public class Calculadora {
+    /**
+     * Este método é ultilizado para somar dois números inteiros
+     * @param numeroUm este é o primeiro parâmetro do metodo
+     * @param numeroDois este é o segundo parâmetro do método
+     * @return int o resultado deste método é a soma dos dois números.
+     */
+    public int somar(int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois; 
+    }
+}     
---
+++ Java-terminal/src/Comentários.java
@@ -0,0 +1,22 @@
+public class Comentários {
+
+    public static void main(String[] args) {
+    //Olá, eu sou um comentário de uma linha
+
+    /* Olá, eu sou um comentário de várias linhas
+    e posso ser utilizado para comentar várias linhas
+    de código
+    */
+    }
+
+    /**
+     * Estas duas estrelinhas acima
+     * é para identificar que você
+     * pretende elaborar um comentário
+     * a nivel de documentação.
+     * Que incrivel!!!!!
+     */
+    public void metodo() {
+        
+    }
+}
