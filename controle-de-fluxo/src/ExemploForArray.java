5820c964b41cbff74eed2bdff9cab8af5f339bd9
Paulo <gabrielbsantana61@gmail.com>
2025-03-13 21:39:20 -0300

    commit diario

---
+++ controle-de-fluxo/src/ExemploForArray.java
@@ -0,0 +1,11 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+        String alunos [] = {"Gabriel", "Anna", "Kauan", "Eduardo"};
+
+        for(String aluno : alunos){
+            System.out.println("Nome do aluno é: " + aluno);
+
+        }
+        }
+    }
+
